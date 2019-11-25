2071c15ccd5c359e241e2ad60268d577eba11c1b
YuengFu <YuengFu.v@outlook.com>
2019-11-26 03:21:50 +0800

    主页更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,59 @@
 module.exports = {
-  title: 'Hello',
-  description: 'Just playing around'
+  temp: './docs/.vuepress/tmp/',
+
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '锈蚀花火',
+      description: '锈蚀花火的个人博客'
+    },
+  },
+
+  themeConfig: {
+    logo: '/logo.ico',
+
+    locales: {
+      '/': {
+        nav: [
+          { text: '文章', link: '/blog/' },
+          { text: 'Github', link: 'https://github.com/TohckyToki/TohckyToki.github.io' },
+        ],
+
+        sidebar: {
+          '/blog/pwsh/': [
+            {
+              title: '返回分类',
+              path: '/blog/',
+            },
+            {
+              title: 'Powershell入门',
+              path: '000001.html',
+            },
+          ],
+          '/blog/other/': [
+            {
+              title: '返回分类',
+              path: '/blog/',
+            },
+            {
+              title: 'GitHub Pages的搭建',
+              path: '000001.html',
+            },
+          ],
+          '/blog/': [
+            {
+              title: 'Powershell',
+              path: '/blog/pwsh/',
+            },
+            {
+              title: '其他',
+              path: '/blog/other/',
+            },
+          ],
+        }
+      }
+    }
+
+  }
+
 }
---
+++ docs/index.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /logo.ico
+heroText: 欢迎来到我的个人博客
+tagline: 这里主要用来分享一些个人学习过程的总结
+actionText: 查看文章列表 →
+actionLink: /blog/
+footer: Copyright © 2019 YuengFu. Built using VuePress
+---
+----
+- 关于作者
+
+  *由于作者是.Net开发出身，所以技术分享主要以.Net和Windows相关为主，大概会包括以下部分，.Net平台的各种语言和开发，Powershell，AHK等各种Windows软件使用*
+- 关于文章
+
+  *由于技术解决方案大多具有时效性，所以不能保证所有内容都完全正确，文章底部会注明最后更新日期，请查看文章之前，先确定内容是否已经过时。如果已经过时，可以尝试联系作者更新，但是不能保证一定会更新。*
