2cfcaeec543cfccd5acfb3ecf76c501bf88983bf
YuengFu <yuengfu.v@outlook.com>
2019-11-28 00:51:51 +0800

    comment 修正

--- docs/.vuepress/components/comment/comment.vue
+++ docs/.vuepress/components/comment/comment.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="gitalk-container">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css">
     <div id="gitalk-container"></div>
-  </div>
+    <script id="script" src="https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js"></script>
 </template>
 <script>
 export default {
@@ -10,12 +10,8 @@ export default {
     return {};
   },
   mounted() {
-    let body = document.querySelector('.gitalk-container');
-    let script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js';
-    body.appendChild(script);
-    script.onload = () => {
-      const commentConfig = {
+    document.querySelector('#script').onload = () => {
+      new Gitalk({
         clientID: '75b92e7b2532bf0770d1',
         clientSecret: '6e3e2e8bb9d0bc09b082e95a699554dc14353c88',
         repo: 'TohckyToki.github.io',
@@ -27,9 +23,7 @@ export default {
         // 但是如果你的 pathname 超过 50 个字符，GitHub 将不会成功创建 issue，此情况可以考虑给每个页面生成 hash 值的方法.
         id: location.pathname,
         distractionFreeMode: false,
-      };
-      const gitalk = new Gitalk(commentConfig);
-      gitalk.render('gitalk-container');
+      }).render('gitalk-container');
     };
   },
 };
